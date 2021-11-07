d5523566755be7d76d486af6ef26f60389136967
tuyuri6ka <m.motoike.kra@gmail.com>
2021-11-08 01:25:28 +0900

    add: zinit and p10k installer

---
+++ bundles/powerlevel-10k.sh
@@ -0,0 +1,2 @@
+echo "powerlevel-10k installed"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
---
+++ bundles/zinit.sh
@@ -0,0 +1,4 @@
+echo "zinit installed"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma-continuum/zinit/master/doc/install.sh)"
+
