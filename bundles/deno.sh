a936dc407212d00759373226a1589d4de3ee1d6e
tuyuri6ka <m.motoike.kra@gmail.com>
2021-11-09 02:40:46 +0900

    add: deno install

--- bundles/deno.sh
+++ bundles/deno.sh
@@ -1,10 +1,4 @@
 echo "deno install"
 
-cargo=`which cargo`
-sudo snap install deno ||
-if [ -z $cargo ]; then
-	cargo install deno --locked
-else
-	#curl -fsSL https://deno.land/x/install/install.sh | sh
-	echo "hoge"
-fi
+#sudo snap install deno
+cargo install deno
